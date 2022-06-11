a026683cb96178a660d5c970e091c15a840e38f2
Stas Sytnykov <stas82kak05@gmail.com>
2022-06-11 14:55:27 +0300

    add firebase settings

--- src/common/firebaseConfig.ts
+++ src/common/firebaseConfig.ts
@@ -1,12 +1,12 @@
 // eslint-disable-next-line import/no-mutable-exports, @typescript-eslint/ban-types
 const firebaseConfig: Object = {
-  apiKey: 'xxx',
-  authDomain: 'xxx',
-  projectId: 'xxx',
-  storageBucket: 'xxx',
-  messagingSenderId: 'xxx',
-  appId: 'xxx',
-  measurementId: 'xxx',
+  apiKey: 'AIzaSyCoG6IpxnO5kKgOYedBnnZdAGQromy1LWc',
+  authDomain: 'mystical-tensor-332213.firebaseapp.com',
+  projectId: 'mystical-tensor-332213',
+  storageBucket: 'mystical-tensor-332213.appspot.com',
+  messagingSenderId: '408678380104',
+  appId: '1:408678380104:web:e1a3178b8a7b8434d09e7d',
+  measurementId: 'G-SJDP8K6MN8',
 };
 
 export default firebaseConfig;
