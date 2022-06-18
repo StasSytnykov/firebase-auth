360e64632cb983035b446f2558bcd1df71172b00
Stas Sytnykov <stas82kak05@gmail.com>
2022-06-18 19:27:36 +0300

    add env file

--- src/common/firebaseConfig.ts
+++ src/common/firebaseConfig.ts
@@ -1,12 +1,12 @@
 // eslint-disable-next-line import/no-mutable-exports, @typescript-eslint/ban-types
 const firebaseConfig: Object = {
-  apiKey: 'AIzaSyCoG6IpxnO5kKgOYedBnnZdAGQromy1LWc',
-  authDomain: 'mystical-tensor-332213.firebaseapp.com',
-  projectId: 'mystical-tensor-332213',
-  storageBucket: 'mystical-tensor-332213.appspot.com',
-  messagingSenderId: '408678380104',
-  appId: '1:408678380104:web:e1a3178b8a7b8434d09e7d',
-  measurementId: 'G-SJDP8K6MN8',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_API_KEY,
+  projectId: process.env.REACT_APP_FIREBASE_API_KEY,
+  storageBucket: process.env.REACT_APP_FIREBASE_API_KEY,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_API_KEY,
+  appId: process.env.REACT_APP_FIREBASE_API_KEY,
+  measurementId: process.env.REACT_APP_FIREBASE_API_KEY,
 };
 
 export default firebaseConfig;
